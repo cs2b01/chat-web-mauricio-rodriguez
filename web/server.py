3ce1476cef844dfa2d2cb7bc0cb5e531a28baa05
mauricio-rodriguez <42778143+mauricio-rodriguez@users.noreply.github.com>
2019-05-29 11:01:03 -0500

    Cambios

--- web/server.py
+++ web/server.py
@@ -16,36 +16,49 @@ def index():
 def static_content(content):
     return render_template(content)
 
+@app.route('/users', methods = ['POST'])
+def create_user():
+    c =  json.loads(request.form['values'])
+    user = entities.User(
+        username=c['username'],
+        name=c['name'],
+        fullname=c['fullname'],
+        password=c['password']
+    )
+    session = db.getSession(engine)
+    session.add(user)
+    session.commit()
+    return 'Created User'
 
 @app.route('/users', methods = ['GET'])
 def get_users():
     session = db.getSession(engine)
     dbResponse = session.query(entities.User)
-    data = []
-    for user in dbResponse:
-        data.append(user)
+    data = dbResponse[:]
     return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
 
+@app.route('/users', methods = ['PUT'])
+def update_user():
+    session = db.getSession(engine)
+    id = request.form['key']
+    user = session.query(entities.User).filter(entities.User.id == id).first()
+    c =  json.loads(request.form['values'])
+    for key in c.keys():
+        setattr(user, key, c[key])
+    session.add(user)
+    session.commit()
+    return 'Updated User'
 
-@app.route('/users/<id>', methods = ['GET'])
-def get_user(id):
-    db_session = db.getSession(engine)
-    users = db_session.query(entities.User).filter(entities.User.id == id)
+@app.route('/users', methods = ['DELETE'])
+def delete_message():
+    id = request.form['key']
+    session = db.getSession(engine)
+    users = session.query(entities.User).filter(entities.User.id == id)
     for user in users:
-        js = json.dumps(user, cls=connector.AlchemyEncoder)
-        return  Response(js, status=200, mimetype='application/json')
-
-    message = { 'status': 404, 'message': 'Not Found'}
-    return Response(message, status=404, mimetype='application/json')
-
-@app.route('/create_test_users', methods = ['GET'])
-def create_test_users():
-    db_session = db.getSession(engine)
-    user = entities.User(name="David", fullname="Lazo", password="1234", username="qwerty")
-    db_session.add(user)
-    db_session.commit()
-    return "Test user created!"
+        session.delete(user)
+    session.commit()
+    return "Deleted User"
 
 if __name__ == '__main__':
     app.secret_key = ".."
-    app.run(port=8080, threaded=True, host=('127.0.0.1'))
+    app.run(port=5000, threaded=True, host=('127.0.0.1'))
--- web/templates/crud_users.js
+++ web/templates/crud_users.js
@@ -1,5 +1,5 @@
 $(function(){
-    var url = "http://127.0.0.1:8080/users";
+    var url = "http://127.0.0.1:5000/users";
 
 
     $("#grid").dxDataGrid({
